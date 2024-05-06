c80ea45c2763d82717341f2c602df8412e42ab53
Archit <architchaturvedi2000@gmail.com>
2024-05-06 21:26:49 +0530

    added schema for users

--- backend/db.js
+++ backend/db.js
@@ -1,9 +1,15 @@
 const mongoose = require('mongoose')
-const mongoURI = 'mongodb+srv://RoyalFood:welcome123@royaldelicacy.hzad4rj.mongodb.net/?retryWrites=true&w=majority&appName=royalDelicacy'
-const mongoDb = ()=>{
+const mongoURI = 'mongodb+srv://RoyalFood:welcome123@royaldelicacy.hzad4rj.mongodb.net/MERNDB?retryWrites=true&w=majority&appName=royalDelicacy'
+const mongoDb = () => {
     mongoose.connect(mongoURI)
-    .then(result=>console.log('connected'))
-    .catch((error)=>console.log(error))
+        .then(async result => {
+            console.log('connected');
+            // const fetched_data = await mongoose.connection.collection("food_items");
+            // fetched_data.find({}).toArray()
+            //     .then(res => console.log(res))
+            //     .catch(err => console.log(err));
+        })
+        .catch((error) => console.log(error))
 }
 
 module.exports = mongoDb
--- backend/index.js
+++ backend/index.js
@@ -3,10 +3,13 @@ const app = express()
 const port = 5000
 const mongoDb = require('./db')
 mongoDb()
+
+app.use(express.json());
+app.use('/api',require('./Routes/CreateUser'))
 app.get('/',(req,res)=>{
     res.send('Hello world')
 })
 
 app.listen(port,()=>{
-    console.log(`My app listening on port ${port}`)
+    console.log(`My app listening on port http://localhost:${port}`)
 })
